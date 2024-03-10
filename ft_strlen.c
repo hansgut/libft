b228fb5b8a20c9462501d743f3e070a38102dfbc
hansgut <yan.tselesh@gmail.com>
2024-03-10 21:51:21 +0100

    add git ignore, change register

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jcielesz <jcielesz@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 00:19:07 by jcielesz          #+#    #+#             */
-/*   Updated: 2024/03/10 21:28:56 by jcielesz         ###   ########.fr       */
+/*   Updated: 2024/03/10 21:50:11 by jcielesz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen(const char *s)
 {
-	const register char	*p;
+	register const char	*p;
 
 	p = s;
 	while (*p)
